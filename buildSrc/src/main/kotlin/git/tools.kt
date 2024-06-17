72431831b8468866b4d1005750eb930e83691674
Ryder Belserion <no-reply@ryderbelserion.com>
2024-06-17 12:54:03 -0400

    [ci-skip] Update git username

--- buildSrc/src/main/kotlin/git/tools.kt
+++ buildSrc/src/main/kotlin/git/tools.kt
@@ -4,7 +4,7 @@ import org.gradle.api.Project
 import java.io.ByteArrayOutputStream
 
 fun formatLog(hash: String, message: String, project: String): String {
-    return "[$hash](https://github.com/Crazy-Crew/$project/commit/$hash) $message<br>"
+    return "[$hash](https://github.com/ryderbelserion/$project/commit/$hash) $message<br>"
 }
 
 fun Project.latestCommitHash(): String {
