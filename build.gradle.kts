24a457555c5c9262afa8a547de68ad834dd4492c
Ryder Belserion <no-reply@ryderbelserion.com>
2024-09-12 15:39:03 -0400

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 val buildNumber: String? = System.getenv("BUILD_NUMBER")
 
-rootProject.version = if (buildNumber != null) "${libs.versions.minecraft.get()}-$buildNumber" else "1.0.0"
+rootProject.version = if (buildNumber != null) "${libs.versions.minecraft.get()}-$buildNumber" else "1.1.0"
 
 val isSnapshot = false
 
