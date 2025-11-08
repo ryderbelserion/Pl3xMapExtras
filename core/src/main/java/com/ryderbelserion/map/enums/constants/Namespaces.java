f4e497501a17ce756e4fcadb9bdfd80979d3d215
Ryder Belserion <contact@ryderbelserion.com>
2025-11-08 17:49:59 -0500

    Update layer namespace keys

--- core/src/main/java/com/ryderbelserion/map/enums/constants/Namespaces.java
+++ core/src/main/java/com/ryderbelserion/map/enums/constants/Namespaces.java
@@ -2,6 +2,8 @@ package com.ryderbelserion.map.enums.constants;
 
 public class Namespaces {
 
-    public static final String banner_key = "pl3xmap_banners";
+    public static final String banner_key = "pl3xmapextras_banners";
+
+    public static final String mob_key = "pl3xmapextras_mobs";
 
 }
